2efaf81c2a23ae18eee8f16d971f06efcd1b4b03
IgorSilvaZZ <igor06silva12@gmail.com>
2023-02-06 17:57:09 -0300

    Refactor register page

--- web/src/pages/Register.tsx
+++ web/src/pages/Register.tsx
@@ -1,10 +1,24 @@
+import firstStepImage from "../assets/first-step-image.png";
+
 export const Register = () => {
   return (
     <div className='w-screen h-screen flex items-center justify-center'>
-      <main className='w-full max-w-5xl h-full max-h-[600px] rounded-lg bg-zinc-800 flex'>
-        <div className='w-[300px] h-full bg-register-image bg-cover bg-no-repeat bg-center'></div>
+      <main className='w-full max-w-7xl h-full max-h-[650px] rounded-lg bg-zinc-800 flex'>
+        <div className='w-[600px] h-full flex flex-col justify-around items-center bg-zinc-700 rounded-l-lg'>
+          <span className='text-4xl text-white leading-tight font-semibold p-5'>
+            Precisamos que você, preencha os campos ao lado para começar!!
+          </span>
 
-        <div className='w-[600px] h-full flex flex-col'></div>
+          <img src={firstStepImage} className='w-[300px]' />
+        </div>
+
+        <div className='w-[600px] mx-auto h-full flex flex-col items-center justify-center gap-2 border'>
+          <input
+            type='text'
+            className='h-14 w-full max-w-md p-3 text-gray-300 font-semibold border-b border-b-zinc-500 bg-transparent outline-none'
+            placeholder='Nome da Academia'
+          />
+        </div>
       </main>
     </div>
   );

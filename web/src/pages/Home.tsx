783d27c1632f921f1b44f7c8163db593e14be5c9
IgorSilvaZZ <igor06silva12@gmail.com>
2023-03-13 23:29:38 -0300

    Adjustment initial page

--- web/src/pages/Home.tsx
+++ web/src/pages/Home.tsx
@@ -5,54 +5,42 @@ import ladingImg from "../assets/lading-page-image.png";
 export const Home = () => {
   return (
     <div className='w-screen h-screen mx-auto flex items-center justify-around'>
-      <main className='w-full max-w-lg flex flex-col gap-3'>
-        <h1 className='mt-14 mb-5 text-white text-5xl font-bold leading-tight'>
+      <main className='w-full max-w-xl flex flex-col gap-3'>
+        <h1 className='mt-14 mb-5 text-white text-6xl font-bold leading-tight'>
           Procure a academia mais proxima de você!!
         </h1>
 
-        <p className='text-gray-300 text-sm leading-relaxed'>
-          Encontre academias mais perto de você, basta procurar pelo seu estado
-          e encontraremos a academias disponiveis. Encontre a academia que mais
-          se encaixa com teu estilo de vida e com teu foco.
+        <p className='text-gray-300 text-base font-medium leading-relaxed'>
+          Encontre academias mais perto de você, use alguns dos filtros
+          disponibilizados abaixo ou clique no botão abaixo.
         </p>
 
-        <div className='flex items-center gap-1 mb-5'>
-          <input
-            type='text'
-            placeholder='Qual seu estado?'
-            className='flex-1 px-4 py-3 outline-none rounded text-gray-300 bg-zinc-800 border border-zinc-600 font-semibold'
-          />
-          <button className='px-4 py-3 bg-violet-600 text-white font-semibold rounded uppercase transition-colors hover:bg-violet-800'>
+        <button className='p-2 rounded-xl mt-4 text-white font-semibold bg-violet-600 max-w-[200px] transition-colors hover:bg-violet-800'>
+          Começa a sua busca!
+        </button>
+
+        <div className='flex items-center gap-3 h-[120px] p-5 mt-8 rounded-lg bg-zinc-900'>
+          <div className='flex flex-col gap-2 max-w-[170px]'>
+            <input
+              type='text'
+              placeholder='Estado'
+              className='p-3 outline-none rounded-md text-gray-300 font-semibold bg-zinc-800'
+            />
+          </div>
+
+          <div className='w-px h-10 bg-violet-600'></div>
+
+          <div className='flex flex-col gap-2 max-w-[170px]'>
+            <input
+              type='text'
+              placeholder='Cidade'
+              className='p-3 outline-none rounded-md text-gray-300 font-semibold bg-zinc-800'
+            />
+          </div>
+
+          <button className='py-3 px-9 bg-violet-600 text-white font-semibold rounded-lg uppercase transition-colors hover:bg-violet-800'>
             Pesquisar
           </button>
-        </div>
-
-        <div className='flex items-center justify-center'>
-          <p className='text-base text-gray-300 font-semibold'>
-            Para Academias
-          </p>
-        </div>
-
-        <div className='pt-10 border-t border-violet-500 flex justify-between items-center text-gray-100'>
-          <div className='flex flex-col gap-3 leading-relaxed items-center justify-center w-80'>
-            <Link
-              to='/loginAcademy'
-              className='py-2 px-10 bg-violet-600 text-white font-semibold rounded-lg transition-colors hover:bg-violet-800'
-            >
-              Logar-se
-            </Link>
-          </div>
-
-          <div className='w-px h-10 bg-violet-500'></div>
-
-          <div className='flex flex-col gap-3 items-center justify-center w-80'>
-            <Link
-              to='/registerAcademy'
-              className='py-2 px-10 bg-violet-600 text-white font-semibold rounded-lg transition-colors hover:bg-violet-800'
-            >
-              Registrar-se
-            </Link>
-          </div>
         </div>
       </main>
 
